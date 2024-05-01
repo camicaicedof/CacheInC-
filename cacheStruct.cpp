97823da1a2f6da9c7ab3360de34cc8ea371ec074
camicaicedof <mccaicedo1975@gmail.com>
2024-05-01 17:51:40 -0500

    Implementamos más funciones :)

--- cacheStruct.cpp
+++ cacheStruct.cpp
@@ -22,6 +22,7 @@ enum class State
     UpdateRam,
     AddBlock,
     ReturnCacheData,
+    ChangeCacheData,
     UpdateBlock,
     PRINTING_RESULTS,
     FINISHED
@@ -87,59 +88,67 @@ private:
     priority_queue<Pair, vector<Pair>, greater<Pair>> leastFrecuentlyUsed;
     vector<string> DRAM;
     int totalHits;
+    bool iniLFU;
 
 public:
-    CacheSimulator() : currentState(State::Initialize), totalHits(0) {}
+    CacheSimulator() : currentState(State::Initialize), totalHits(0), iniLFU(true) {}
 
     void transition(State nextState)
     {
         currentState = nextState;
     }
 
-    void events(int memData, int blockId = -1)
+    void events(int memData, string instruction, string dataChange, int blockId = -1)
     {
         switch (currentState)
         {
         case State::Initialize:
             cout << "Initialize" << endl;
-            initialize(memData);
+            initialize(memData, instruction, dataChange);
             break;
         case State::ReadAddress:
             cout << "read address" << endl;
-            readData(memData);
+            readData(memData, instruction, dataChange);
             break;
         case State::CheckHit:
             cout << "CheckHit" << endl;
-            checkHit(memData);
+            checkHit(memData, instruction, dataChange);
             break;
         case State::ReadInstruction:
             cout << "ReadInstruction" << endl;
-            checkHit(memData);
+            readInstruction(memData, instruction, dataChange, blockId);
             break;
         case State::CheckMemorySize:
             cout << "CheckMemorySize" << endl;
-            checkMemorySize(memData);
+            checkMemorySize(memData, instruction, dataChange);
             break;
         case State::ApplyLFU:
-            // applyLFU(memData);
+            cout << "ApplyLFU" << endl;
+            applyLFU(memData, instruction, dataChange, blockId);
             break;
         case State::CheckCacheForDB:
             cout << "CheckCacheForDB" << endl;
-            checkCacheForDB(memData);
+            checkCacheForDB(memData, instruction, dataChange);
             break;
         case State::UpdateRam:
-            updateRam(memData, blockId);
+            cout << "UpdateRam" << endl;
+            updateRam(memData, instruction, dataChange, blockId);
             break;
         case State::UpdateBlock:
-            // updateBlock(memData);
+            cout << "UpdateBlock" << endl;
+            updateBlock(memData, instruction, dataChange, blockId);
             break;
         case State::AddBlock:
             cout << "AddBlock" << endl;
-            addBlock(memData);
+            addBlock(memData, instruction, dataChange);
             break;
         case State::ReturnCacheData:
             cout << "ReturnCacheData" << endl;
-            // returnCacheData(memData);
+            returnCacheData(memData, instruction, dataChange, blockId);
+            break;
+        case State::ChangeCacheData:
+            cout << "ChangeCacheData" << endl;
+            changeCacheData(memData, instruction, dataChange, blockId);
             break;
         case State::PRINTING_RESULTS:
             printResults();
@@ -150,7 +159,7 @@ public:
         }
     }
 
-    void initialize(int memData)
+    void initialize(int memData, string instruction, string dataChange)
     {
         string line;
         ifstream RAM("DRAM.txt");
@@ -165,21 +174,21 @@ public:
             getline(RAM, line);
             DRAM.push_back(line);
         }
-        events(memData);
+        events(memData, instruction, dataChange);
     }
 
-    void readData(int memData)
+    void readData(int memData, string instruction, string dataChange)
     {
         if (memData < 3072)
         {
             transition(State::CheckHit);
-            events(memData);
+            events(memData, instruction, dataChange);
         }
         else
             printf("Dir > 3072\n");
     }
 
-    void checkHit(int memData)
+    void checkHit(int memData, string instruction, string dataChange)
     {
         string memDataS, blockAdd;
         memDataS = decToBin(memData);
@@ -198,19 +207,19 @@ public:
         {
             transition(State::CheckMemorySize);
         }
-        events(memData);
+        events(memData, instruction, dataChange);
     }
 
-    void checkMemorySize(int memData)
+    void checkMemorySize(int memData, string instruction, string dataChange)
     {
         if (memory.size() < 64)
             transition(State::CheckCacheForDB);
         else
             transition(State::ApplyLFU);
-        events(memData);
+        events(memData, instruction, dataChange);
     }
 
-    void addBlock(int memData)
+    void addBlock(int memData, string instruction, string dataChange)
     {
         // Add block to cache memory
         Block newBlock;
@@ -230,17 +239,15 @@ public:
         for (int i = 0; i < 32; i++)
         {
             newBlock.data.push_back(DRAM[i + newBlock.leftLimit]);
-            cout << DRAM[i + newBlock.leftLimit] << endl;
         }
         // Initialize newEntry
         memory.push_back(newBlock);
         isHit[blockAdd] = newBlock.id;
-        // DE MOMENTO ACABA AQUÍIIIIIIIIIIIII
-        transition(State::FINISHED);
-        events(memData);
+        transition(State::ReadInstruction);
+        events(memData, instruction, dataChange, newBlock.id);
     }
 
-    void checkCacheForDB(int memData)
+    void checkCacheForDB(int memData, string instruction, string dataChange)
     {
         bool flag = true;
         int i = 0;
@@ -255,10 +262,83 @@ public:
             transition(State::UpdateRam);
         else
             transition(State::AddBlock);
-        events(memData, i);
+        events(memData, instruction, dataChange, i);
+    }
+    
+    void readInstruction(int memData, string instruction, string dataChange, int id){
+        if (instruction == "Rd")
+            transition(State::ReturnCacheData);
+        else if (instruction == "Wr")
+            transition(State::ChangeCacheData);
+        events(memData, instruction, dataChange, id);
     }
 
-    void updateRam(int memData, int id)
+    void returnCacheData(int memData, string instruction, string dataChange, int id){
+        //En caso de que se pete, poner el offset jaja
+        cout << memory[id].data[memData-memory[id].leftLimit]<<endl;
+        transition(State::ReadAddress);
+    }
+
+    void changeCacheData(int memData, string instruction, string dataChange, int id){
+        memory[id].data[memData-memory[id].leftLimit] = dataChange;
+        memory[id].dirtyBit = true;
+        transition(State::ReadAddress);
+    }
+
+    void updateBlock(int memData, string instruction, string dataChange, int id)
+    {
+        string memDataS, blockAdd, offset;
+
+        memDataS = decToBin(memData);
+        blockAdd = memDataS.substr(0, 7);
+        offset = memDataS.substr(7, 5); // Offset de 5 bits
+        // Update keys for Hits
+        isHit.erase(memory[id].blockAddress);
+        isHit[blockAdd] = id;
+        // Update the block
+        memory[id].blockAddress = blockAdd;
+        memory[id].validBit = true;
+        memory[id].dirtyBit = false;
+        memory[id].leftLimit = memData - binToInt(offset);
+        memory[id].rightLimit = memory[id].leftLimit + 31;
+
+        for (int i = 0; i < 32; i++)
+            memory[id].data.push_back(DRAM[i + memory[id].leftLimit]);
+        transition(State::ReadInstruction);
+        events(memData, instruction, dataChange, id);
+    }
+    
+    void applyLFU(int memData, string instruction, string dataChange, int id)
+    {
+        if (iniLFU)
+        {
+            for (int j = 0; j < memory.size(); j++)
+            {
+                pair<int, int> tmp(memory[j].hitCount, j);
+                leastFrecuentlyUsed.push(tmp);
+            }
+            iniLFU = false;
+        }
+        bool flag = true;
+        int numBlock;
+        while (flag)
+        {
+            Pair tmp = leastFrecuentlyUsed.top();
+            leastFrecuentlyUsed.pop();
+            if (tmp.first == memory[tmp.second].hitCount)
+            {
+                numBlock = tmp.second;
+                flag = false;
+            }
+            else
+            {
+                pair<int, int> p(memory[tmp.second].hitCount, tmp.second);
+                leastFrecuentlyUsed.push(p);
+            }
+        }
+    }
+
+    void updateRam(int memData, string instruction, string dataChange, int id)
     {
         int pos = memory[id].leftLimit;
         for (int i = 0; i < 32; i++)
@@ -268,7 +348,8 @@ public:
         if (memory.size() < 64)
             transition(State::AddBlock);
         else
-            transition(State::AddBlock);
+            transition(State::UpdateBlock);
+        events(memData, instruction, dataChange, id);
     }
 
     void printResults()
@@ -277,17 +358,21 @@ public:
         // Transition to FINISHED state
         transition(State::FINISHED);
     }
-};
+};  
 
 int main()
 {
     CacheSimulator cacheSimulator;
-
-    int memData;
-    while (cin >> memData && memData != '#')
-    {
-        cacheSimulator.events(memData);
+    int address;
+    string RoW, change;
+    while (cin>>RoW)
+    {   cin >> address;
+        if (RoW == "Wr"){
+            cin >> change;
+        }
+        
+        cacheSimulator.events(address, RoW, change);
+    
     }
-
     return 0;
 }
